e28251b8a594cad630c62f73064294da077a8a0d
Siddharth Verma <siddharthverma3904@gmail.com>
2023-04-12 19:06:34 +0530

    added label for wlcm msg

--- casper.py
+++ casper.py
@@ -16,14 +16,6 @@ bg_mode = 'dark'
 ctk.set_appearance_mode(bg_mode)
 ctk.set_default_color_theme('green')
 
-def speak(msg) :
-    for i in msg :
-        print(i, end='')
-        time.sleep(0.005)
-    print('\n')
-    CASPER.spkr.say(msg)
-    CASPER.spkr.runAndWait()
-
 class Assistant(ctk.CTk):
     def __init__(self, name, owner, wakeword):
         super().__init__()
@@ -49,54 +41,55 @@ class Assistant(ctk.CTk):
         self.grid_rowconfigure((0, 1, 2), weight=1)
 
         self.frame = ctk.CTkFrame(
-            master=self, 
+            master=self,
             corner_radius=10)
         self.frame.pack(pady=20, padx=60, fill="both", expand=True)
 
         self.name_display = ctk.CTkLabel(
-            master=self.frame, 
-            justify=ctk.LEFT,
-            text='C.A.S.P.E.R', 
+            master=self.frame,
+            text='C.A.S.P.E.R',
             font=ctk.CTkFont(size=50, family=self.font))
         self.name_display.pack(pady=10, padx=10)
         
-        self.imag=ImageTk.PhotoImage(Image.open('C:/Users/DELL/Desktop/sid/projects/casper/costume1.png'))
+        self.imag = ImageTk.PhotoImage(Image.open('C:/Users/DELL/Desktop/sid/projects/casper/costume1.png'))
         self.wake_button = ctk.CTkButton(
-            master=self.frame, 
-            image=self.imag, 
+            master=self.frame,
+            image=self.imag,
             text=None,
             font=(self.font, 80),
             fg_color='transparent',
-            command=self.change_bg,
+            command=self.wake,
             hover=False)
         self.wake_button.pack()
 
-    # defining actions
+        self.chat = ctk.CTkLabel(
+            master = self.frame,
+            corner_radius = 10,
+            height = 50,
+            width = 500,
+            text = None,
+            justify='left',
+            anchor = 'w',
+            font=ctk.CTkFont(size=15, family=self.font))
+        self.chat.place(relx=0, anchor='w') # move the text to the left side of frame
+        self.chat.place(x=0, y=0)
+        self.chat.pack(padx=10, pady=10)
 
-    def change_bg(self) :
-        global bg_mode
-        if bg_mode == 'dark' :
-            bg_mode = 'light'
-        else :
-            bg_mode = 'dark'
-        ctk.set_appearance_mode(bg_mode)
-        self.wake_button.destroy()
-        self.update()
-        self.wish()
-        self.wake()
-        
-    def wish(self) :
+    # defining actions
+    def welcome(self) :
         os.system('cls')
         hr=self.hr%12
         if hr==0 :
             hr=12
-        mnt=self.mnt
+        mnt=str(self.mnt)
+        if(self.mnt<10) :
+            mnt = '0'+str(mnt)
         if(self.hr<12 and self.hr>0) :
-            msg=f"Good-morning {self.owner}, {self.name} here, it's {str(hr)}:{str(mnt)} AM"
+            msg=f"Good-morning {self.owner}, {self.name} here, it's {str(hr)}:{mnt} AM"
         elif(self.hr>=12 and self.hr<17) :
-            msg=f"Good-afternoon {self.owner}, {self.name} here, it's {str(hr)}:{str(mnt)} PM"
+            msg=f"Good-afternoon {self.owner}, {self.name} here, it's {str(hr)}:{mnt} PM"
         else :
-            msg=f"Good-evening {self.owner}, {self.name} here, it's {str(hr)}:{str(mnt)} PM"
+            msg=f"Good-evening {self.owner}, {self.name} here, it's {str(hr)}:{mnt} PM"
         speak(msg)
 
     def listen_cmd(self) :
@@ -117,6 +110,11 @@ class Assistant(ctk.CTk):
                 continue
         
     def passive(self) :
+        ctk.set_appearance_mode('light')
+        self.wake_button.destroy()
+        self.chat.configure(fg_color = 'grey')
+        self.update()
+        self.welcome()
         while True :
             os.system('cls')
             print('...')
@@ -133,6 +131,7 @@ class Assistant(ctk.CTk):
                         if 'sleep' in command.lower() :
                             speak('ok, see ya !')
                             os._exit(0)
+                        speak("yes sir !")
                         return
             except :
                 continue
@@ -146,6 +145,16 @@ class Assistant(ctk.CTk):
             self.passive()
             command=self.listen_cmd()
             self.respond(command)
+
+def speak(msg) :
+    txt=''
+    for i in msg :
+        txt+=i
+        CASPER.chat.configure(text=txt)
+        time.sleep(0.01)
+        CASPER.update()
+    CASPER.spkr.say(msg)
+    CASPER.spkr.runAndWait()
 
 #doing actions
 if __name__ == "__main__":
